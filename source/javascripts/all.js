e60dd828ce02f42e06f036544700bd1cd0317b99
carolineartz <ceartz@gmail.com>
2015-07-05 22:22:12 -0500

    begins some more serious changes

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -15,3 +15,53 @@ $(document).ready(function() {
     });
   });
 });
+
+
+jQuery(document).ready(function($){
+	//move nav element position according to window width
+	moveNavigation();
+	$(window).on('resize', function(){
+		(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+	});
+
+	//mobile version - open/close navigation
+	$('.nav-trigger').on('click', function(event){
+		event.preventDefault();
+		if($('header').hasClass('nav-is-visible')) $('.moves-out').removeClass('moves-out');
+		
+		$('header').toggleClass('nav-is-visible');
+		$('.main-nav').toggleClass('nav-is-visible');
+		$('.main-content').toggleClass('nav-is-visible');
+	});
+
+	//mobile version - go back to main navigation
+	$('.go-back').on('click', function(event){
+		event.preventDefault();
+		$('.main-nav').removeClass('moves-out');
+	});
+
+	//open sub-navigation
+	$('.subnav-trigger').on('click', function(event){
+		event.preventDefault();
+		$('.main-nav').toggleClass('moves-out');
+	});
+
+	function moveNavigation(){
+		var navigation = $('.main-nav-wrapper');
+  		var screenSize = checkWindowWidth();
+        if ( screenSize ) {
+        	//desktop screen - insert navigation inside header element
+			navigation.detach();
+			navigation.insertBefore('.nav-trigger');
+		} else {
+			//mobile screen - insert navigation after .main-content element
+			navigation.detach();
+			navigation.insertAfter('.main-content');
+		}
+	}
+
+	function checkWindowWidth() {
+		var mq = window.getComputedStyle(document.querySelector('header'), '::before').getPropertyValue('content').replace(/"/g, '').replace(/'/g, "");
+		return ( mq == 'mobile' ) ? false : true;
+	}
+});
